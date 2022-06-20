7e14844071c1d04020ac2eb7e2c1011d3beaf26a
caio-ortega-morais <caio.ortega.morais@gmail.com>
2022-06-20 17:15:07 -0300

    Fluxo_caixaDAO e Create implementado

--- src/com/icarros/global/QueryUtils.java
+++ src/com/icarros/global/QueryUtils.java
@@ -5,4 +5,7 @@ public class QueryUtils {
 	public static final String QUERY_GET_ALL_CORRENTISTA = "SELECT * FROM correntista.correntista";
 
 	public static final String QUERY_GET_CORRENTISTA_BY_ID = "SELECT * FROM correntista.correntista where id = %d";
+	
+	public static final String INSERT_FLUXO_CAIXA = "INSERT INTO correntista.fluxo_caixa VALUES"
+													+ " (%d, %d, %d, %s, %s);";
 }
